8e7f28f62fdc299d6ac62efd3ab3ad5631c60f6f
David Foster <dlfosterii@yahoo.com>
2020-04-07 19:00:42 -0400

    menu created

--- room_app_ex.py
+++ room_app_ex.py
@@ -15,14 +15,25 @@ guest_info = {
     'prepaid?' : False
 }
 
-
 guest_info = {
     'occupant name' : "James",
     'phone number' : '770-555-3235',
     'prepaid?' : False
 }
+menu = '''
+
+1. Check-in new guest
+2. Check-out guest
+3. Check room availability
+
+'''
+
+print('Guest check-in/check-out system')
+
+
 guest_info['occupant name'] = input('Guest name? ')
 guest_info['phone number'] = input('Phone number? ')
+guest_info['prepaid?'] = input('Has guest prepaid for their room? ')
 
 
 room_assign = input('Assign to which room? ')
@@ -31,6 +42,51 @@ print(hotel)
 
 #this will prompt for phone and assign to the key 'phone number for new_guest
 
+
+
+# Exercise instructions...
+
+# Small: Single hotel
+
+
+# The goal of the small exercise is to get practice with the syntax for querying and manipulating the data in a single, nested dictionary.
+
+# Write functions to:
+
+# - is_vacant(which_hotel, '101')
+#     - check if a room is occupied
+# - check_in('101', guest_dictionary)
+#     - assign a person to a room
+# - check_out('101')
+#     - returns the person dictionary in that room
+
+# Please look back at any notes or slides for how to perform any of these actions.
+
+# Medium: three hotels
+
+# First, create a list of hotel dictionaries. They should contain at least 3 identical dictionaries (like the one in the example above).
+
+# Create a while True loop to show the following menu:
+
+
+# 1. Print hotel room status
+# 2. Check in customer
+# 3. Check out customer
+# 4. Quit
+
+# - When printing, show all rooms for all hotels and the name of the occupant, if there is one.
+# - When checking a customer in, make sure to choose a hotel as well as a room.
+# - Do not let a customer check into an occupied room.
+# - If the room is unoccupied, prompt for each piece of information (name, cell, etc.)
+# - upon check out, print out whether or not the customer has paid
+
+# ```
+
+# Medium: add/remove hotels
+
+# Add more options to your main menu.
+
+# Provide options to open a new hotel or close an existing hotel.
 
 
 
